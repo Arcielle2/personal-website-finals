7046b0d1ba9732a8fa63890d6fcac3e5ce3df10d
ArcielleGercan <acgercan@student.apc.edu.ph>
2025-03-01 10:27:56 +0000

    restart

--- home/src/App.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <Book/>  
-
-</template>
-
-<script></script>
--- home/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import Book from './components/Book.vue'
-
-createApp(App).mount('#app');
-app.component('book', Book)

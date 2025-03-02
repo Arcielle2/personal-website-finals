e1e6e7839d12ece34b5660d456c56200618fcec2
ArcielleGercan <acgercan@student.apc.edu.ph>
2025-03-02 06:35:29 +0000

    try??

--- home/src/components/Book.vue
+++ home/src/components/Book.vue
@@ -1,6 +1,48 @@
 <template>
-  hi
+  <header class="header">
+    <a href="#home" class="logo"><span>Arcielle Marie</span></a>
+    <i class="bx bx-menu" id="menu-icon"></i>
+
+    <nav class="navbar">
+      <a href="#home" class="active">Home</a>
+      <a href="#education">Education</a>
+      <a href="#course">Course</a>
+      <a href="#experience">Experience</a>
+      <a href="#goals">Goals in Life</a>
+      <a href="#hobbies">Hobbies and Interests</a>
+      <a href="#picture">Picture Gallery</a>
+    </nav>
+  </header>
+
+  <section class="home">
+    <div class="home-img">
+      <img 
+        src="https://arciellegercan.github.io/WEBPROG-SF231-Personal-Website/home/pictures/Media%20(3).jfif" 
+        alt="Homepage Picture"
+      />
+    </div>
+    <div class="home-content">
+      <h1>Hi, I'm <span>Arcielle!</span></h1>
+      <h3 class="text-animation">I'm a <span></span></h3>
+      <p>
+        My name is Arcielle Marie Gercan, you can call me Arcielle or Cielle! <br />
+        I’m from Bulacan, and I have a fun, easy-going, and cheerful personality.
+        I enjoy trying new things and finding exciting experiences, whether it’s learning something new or meeting great people like you.
+        Feel free to explore my page!
+      </p>
+      <div class="social-icons">
+        <a href="https://www.linkedin.com/in/arcielle-marie-gercan/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+        <a href="https://github.com/ArcielleGercan" target="_blank"><i class="fa-brands fa-github"></i></a>
+        <a href="https://www.facebook.com/arciellemcg/" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+        <a href="https://www.instagram.com/arxchmvri/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+      </div>
+      <a href="#education" class="btn">Get to know me more!</a>
+      <p class="quote">~ Venture outside your comfort zone. The rewards are worth it ~</p>
+      <p class="quote-author">— Rapunzel, *Tangled*</p>
+    </div>
+  </section>
 </template>
 
 <style scoped>
+/* Add necessary styles here */
 </style>

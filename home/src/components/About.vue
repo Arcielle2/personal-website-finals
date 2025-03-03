5f540d18cf27f35477ef9086f20b7a70df1da651
ArcielleGercan <acgercan@student.apc.edu.ph>
2025-03-03 11:04:09 +0000

    try?

--- home/src/components/About.vue
+++ home/src/components/About.vue
@@ -264,7 +264,7 @@ onUnmounted(() => {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: whitesmoke;
+  background-color: rgba(245, 245, 245, 0.877);
   border-radius: 12px;
   display: flex;
   flex-direction: column;
